8c930a63fac62fa935959bff3ce2faa24e3a6c8e
heysandy <sundeeppoudel@gmail.com>
2025-01-23 17:23:39 -0500

    Initial Commit

---
+++ LSP_SPRING_2025/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,13 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+/**
+ * Gorakh Khatri
+ * @param args
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
